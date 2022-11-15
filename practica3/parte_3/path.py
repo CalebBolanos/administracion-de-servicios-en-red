0604039255f2c69fc7934cd309fdd6eef9d8668f
Caleb Bolaños Ramos <bolanos.c@hotmail.com>
2022-11-15 08:16:33 -0600

    empezando practica 3

---
+++ practica3/parte_3/path.py
@@ -0,0 +1,4 @@
+rrdpath = '/home/caleb/Documentos/GitHub/administracion-de-servicios-en-red/practica3/RRD/'
+pngpath = '/home/caleb/Documentos/GitHub/administracion-de-servicios-en-red/practica3/IMG/'
+rrdname= "netP.rrd"
+pngfname="predict.png"
