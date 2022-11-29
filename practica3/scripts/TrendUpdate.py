bd0f93abdde5725f0b342b259748c4d908052f17
Caleb Bolaños Ramos <bolanos.c@hotmail.com>
2022-11-28 22:47:14 -0600

    trend update completo

--- practica3/scripts/TrendUpdate.py
+++ practica3/scripts/TrendUpdate.py
@@ -2,14 +2,48 @@ import time
 import rrdtool
 from getSNMP import consultaSNMP
 rrdpath = '/home/caleb/Documentos/GitHub/administracion-de-servicios-en-red/practica3/RRD/'
-carga_CPU = 0
+
+oids_cpu = {
+    'cpu1': '1.3.6.1.2.1.25.3.3.1.2.196608',
+    'cpu2': '1.3.6.1.2.1.25.3.3.1.2.196609',
+    'cpu3': '1.3.6.1.2.1.25.3.3.1.2.196610',
+    'cpu4': '1.3.6.1.2.1.25.3.3.1.2.196611',
+}
+
+oid_ram_total = '1.3.6.1.4.1.2021.4.5.0'
+oid_ram_libre = '1.3.6.1.4.1.2021.4.6.0'
+
+oids_octetos_entrada = {
+    'octeto1': '1.3.6.1.2.1.2.2.1.10.1',
+    'octeto2': '1.3.6.1.2.1.2.2.1.10.2',
+}
+oids_octetos_salida= {
+    'octeto1': '1.3.6.1.2.1.2.2.1.16.1',
+    'octeto2': '1.3.6.1.2.1.2.2.1.16.2',
+}
+
 
 while 1:
-    cpu_uno = int(consultaSNMP('comunidadASR','localhost','1.3.6.1.2.1.25.3.3.1.2.196608'))
-    cpu_dos = int(consultaSNMP('comunidadASR','localhost','1.3.6.1.2.1.25.3.3.1.2.196609'))
-    promedio = (cpu_uno + cpu_dos)/2
-    print ("N:" + str(cpu_uno) + ":" + str(cpu_dos) + ":" + str(promedio))
-    valor = "N:" + str(promedio)
+    #carga de cpu
+    suma = 0
+    for cpu in oids_cpu:
+        suma += int(consultaSNMP('comunidadASR','localhost',oids_cpu[cpu]))
+
+    carga_cpu = (suma)/len(oids_cpu)
+
+    #carga de ram en kb
+    carga_ram = int(consultaSNMP('comunidadASR','localhost', oid_ram_total)) - int(consultaSNMP('comunidadASR','localhost', oid_ram_libre))
+
+    #carga de red (total de octetos)
+    suma_red = 0
+    for octetos_in in oids_octetos_entrada:
+        suma_red += int(consultaSNMP('comunidadASR', 'localhost', oids_octetos_entrada[octetos_in]))
+    for octetos_out in oids_octetos_salida:
+        suma_red += int(consultaSNMP('comunidadASR', 'localhost', oids_octetos_salida[octetos_out]))
+
+    carga_red = suma_red
+
+    valor = "N:" + str(carga_cpu) + ":" + str(carga_ram) + ":" + str(carga_red)
     print(valor)
     rrdtool.update(rrdpath+'trend.rrd', valor)
     time.sleep(5)
--- practica3/scripts/trendCreate.py
+++ practica3/scripts/trendCreate.py
@@ -1,8 +1,10 @@
 import rrdtool
-ret = rrdtool.create("/home/caleb/Documentos/ejercicios/Introduccion_SNMP-master/6-AdministraciónDeRendimiento/RRD/trend.rrd",
+ret = rrdtool.create("/home/caleb/Documentos/GitHub/administracion-de-servicios-en-red/practica3/RRD/trend.rrd",
                      "--start",'N',
                      "--step",'60',
                      "DS:CPUload:GAUGE:60:0:100",
+                     "DS:RAMload:GAUGE:60:0:100",
+                     "DS:NETWORKload:GAUGE:60:0:100",
                      "RRA:AVERAGE:0.5:1:24")
 if ret:
     print (rrdtool.error())
